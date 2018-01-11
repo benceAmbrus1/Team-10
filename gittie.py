5be219ba352d0f46a4cfb6b06b7f81199a7dbcf2
Tamas Kiss <sauriaster@gmail.com>
2018-01-11 09:30:16 +0100

    shout implemented

--- gittie.py
+++ gittie.py
@@ -6,7 +6,6 @@ def add(a, b):
     return a + b
 def joke():
     print("This code is a joke!")
-<<<<<<< HEAD
 
-=======
->>>>>>> 821d047f0acf2cb83102fcb606fcf655d3f790dd
+def shout():
+    print("THIS IS A SHOUTING BITH< CAN'T YOU HEAR IT?")
