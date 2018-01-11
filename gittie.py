f17143409b8bed7fa9056f004eb752f5d63c6e75
Tamas Kiss <sauriaster@gmail.com>
2018-01-11 09:24:26 +0100

    modified introduce fn

--- gittie.py
+++ gittie.py
@@ -1,11 +1,9 @@
 def introduce():
-    print("Hello, i'm Gittie")
+    print("Hello, i'm Gittie and i want to kill you all")
 
-<<<<<<< HEAD
 
 def add(a, b):
     return a + b
-=======
 def joke():
     print("This code is a joke!")
->>>>>>> 6f8426747dc538e5e4a89249d274f1a64def4db0
+
